bc3dd1ead0a8863b49642a9150fcb1f5b16299d1
Arief Setya <ariefsetya@live.com>
2017-04-19 14:19:32 +0700

    update

--- resources/views/blogs-objek.blade.php
+++ resources/views/blogs-objek.blade.php
@@ -5,49 +5,49 @@
 <div class="grid">
 	@for($i=0;$i<ceil(sizeof($search)/$point);$i++)
 	<div class="row cells{{$point}}">
-	@for($x=($i>0?1:0);$x<$point+($i>0?1:0);$x++)
-	@if(isset($search[$i+$x]))
+	@for($x=$i;$x<$point+$i;$x++)
+	@if(isset($search[$x]))
 		<div class="cell">
 			<div class="panel">
 			    <div class="heading">
-			        <span class="title">{{$search[$i+$x]->nama}}</span>
+			        <span class="title">{{$search[$x]->nama}}</span>
 			    </div>
 			    <div class="content">
-			        <img src="{{$search[$i+$x]->photo}}">
+			        <img src="{{$search[$x]->photo}}">
 			        <div class="padding10">
 				        <hr>
 				        <table class="table striped hovered bordered">
 				        	<tr>
 				        		<td>Creator</td>
-				        		<td>{{$search[$i+$x]->creator}}</td>
+				        		<td>{{$search[$x]->creator}}</td>
 			        		</tr>
-			        		@if($search[$i+$x]->reskin!=null)
+			        		@if($search[$x]->reskin!=null)
 				        	<tr>
 				        		<td>Reskin</td>
-				        		<td>{{$search[$i+$x]->reskin}}</td>
+				        		<td>{{$search[$x]->reskin}}</td>
 			        		</tr>
 			        		@endif
 				        	<tr>
 				        		<td>Status</td>
 				        		<td>
-				        		@if($search[$i+$x]->status=="plu")
+				        		@if($search[$x]->status=="plu")
 				        			Private Limited User
-				        		@elseif($search[$i+$x]->status=="free")
+				        		@elseif($search[$x]->status=="free")
 				        			Freeware
-				        		@elseif($search[$i+$x]->status=="pay")
+				        		@elseif($search[$x]->status=="pay")
 				        			Payware
 				        		@endif
 				        		</td>
 			        		</tr>
 				        	<tr>
-				        		<td colspan="2"  style="word-wrap: break-word;white-space:pre-wrap;max-width:100px;">Description<br><br>{!!$search[$i+$x]->description or '--No Description--'!!}</td>
+				        		<td colspan="2"  style="word-wrap: break-word;white-space:pre-wrap;max-width:100px;">Description<br><br>{!!$search[$x]->description or '--No Description--'!!}</td>
 				        	</tr>
 				        </table>
 				        <div dir="rtl" >
-				        	@if(Auth::check() and $search[$i+$x]->status!="free" or $search[$i+$x]->status=="free")
-				        	<a class="text-right button primary" href="{{route('link_user_objek',[base64_encode($search[$i+$x]->id)])}}">Download</a>
+				        	@if(Auth::check() and $search[$x]->status!="free" or $search[$x]->status=="free")
+				        	<a class="text-right button primary" href="{{route('link_user_objek',[base64_encode($search[$x]->id)])}}">Download</a>
 				        	@else
-				        	<a class="text-right button primary" href="{{route('link_user_objek',[base64_encode($search[$i+$x]->id)])}}"><span>Masuk atau Daftar untuk download</span></a>
+				        	<a class="text-right button primary" href="{{route('link_user_objek',[base64_encode($search[$x]->id)])}}"><span>Masuk atau Daftar untuk download</span></a>
 				        	@endif
 				        </div>
 			        </div>
