fa6b13c458c4561c07cc438d847a3594df71fb38
Arief Setya <ariefsetya@live.com>
2017-05-12 02:43:20 +0700

    add terms

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,32 @@
+@extends('local')
+
+@section('content')
+
+<h2>Indonesian Trainz X - Terms of Service</h2>
+
+<ol type="1">
+<li>Indonesian Trainz X adalah website portal penyedia konten Kereta Simulator Indonesia (Trainz Simulator).</li>
+<li>Indonesian Trainz X menerima pendaftaran untuk PLU Member dengan data yang valid berikut ini.
+<ol type="a">
+<li>Nama</li>
+<li>E-Mail</li>
+<li>Nomor Telepon</li>
+<li>Facebook Username</li>
+<li>Foto</li>
+<li>Pernyataan</li>
+<li>Data Trainz yang terinstall</li>
+<li>Nama dan Website Creator/Reskiner (jika ada) apabila yang bersangkutan merupakan Creator/Reskiner.</li>
+</ol>
+</li>
+<li>PLU Member adalah user yang diterima oleh sistem ITX dengan data-data yang valid pada saat pendaftaran.</li>
+<li>PLU Member akan ditolak secara otomatis apabila data yang diberikan tidak valid.</li>
+<li>PLU Member yang sudah mendaftar akan mendapatkan konfirmasi diterima/ditolak melalui email.</li>
+<li>PLU Member berhak mendapatkan konten-konten yang bersifat PLU atau beta tester yang dikirim melalui email.</li>
+<li>Konten ITX adalah keseluruhan konten yang ada di website ITX baik yang bersifat Freeware, PLU atau Payware.</li>
+<li>Konten ITX dilarang untuk di konvert ke Trainz Android.</li>
+<li>Konten ITX dilarang untuk dijual kembali dalam bentuk apapun.</li>
+<li>Konten ITX diperbolehkan untuk di reskin maupun diedit, akan tetapi dilarang untuk di upload ulang dalam bentuk CDP/CDP2.</li>
+<li>Konten ITX yang bersifat Freeware atau PLU diperbolehkan untuk digunakan dalam Rute Payware/PLU dengan syarat pihak ITX mendapatkan Rute tersebut. ( baca : kirim ke <a href="mailto:itx.officialmail@gmail.com">itx.officialmail@gmail.com</a> )</li>
+<li>Konten ITX yang bersifat Freeware atau PLU diperbolehkan untuk digunakan dalam rute Private tanpa perlu izin terlebih dahulu.</li>
+</ol>
+@endsection
