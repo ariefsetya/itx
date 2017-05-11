a67a3e76e64105405d8dc8f456264beba20596e0
Arief Setya <ariefsetya@live.com>
2017-05-12 04:50:19 +0700

    update cu

--- resources/views/member/premium_user.blade.php
+++ resources/views/member/premium_user.blade.php
@@ -14,21 +14,23 @@
 						<th>E-Mail</th>
 						<th>Phone</th>
 						<th>ORI?</th>
+						<th>CU?</th>
 					</tr>
 				</thead>
 				<tbody>
 				@foreach($user as $row)
-					<?php //@if(sizeof(\App\UserContent::where('id_assign',$row->id)->where('id_content',$konten->id)->get())==1)?>
 						<tr>
 							<td>#{{$row->id}}</td>
 							<td>{{$row->name}}</td>
 							<td>{{$row->email}}</td>
 							<td>{{$row->phone}}</td>
 							<td>{{$row->ori==1?"Ya":"Tidak"}}</td>
+							<td>@if(sizeof(\App\UserContent::where('id_assign',$row->id)->where('id_content',$konten->id)->get())==1)
+							Ya @else Tidak @endif</td>
 						</tr>
 						@if($row->brand!=null)
 						<tr>
-							<td colspan="5">
+							<td colspan="6">
 								<h2>{{$row->brand}}</h2>
 								<hr>
 								-->> <a target="_blank" href="{{$row->url}}">{{$row->url}}</a>
@@ -58,10 +60,9 @@
 								<a class="button success" href="{{route('kirim_konten',[$row->id,$konten->id,$type])}}">Kirim</a>
 							</td>
 						</tr>
-					<?php //@endif ?>
 				@endforeach
 				@if(sizeof($user)==0)
-					<tr><td colspan="5"><h1><span class="mif-heart-broken mif-4x"></span> Belum ada data</h1></td></tr>
+					<tr><td colspan="6"><h1><span class="mif-heart-broken mif-4x"></span> Belum ada data</h1></td></tr>
 				@endif
 				</tbody>
 			</table>
