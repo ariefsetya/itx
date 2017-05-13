0ebdd4ea9720536d8b29c08a9fb94d3986e94193
Arief Setya <ariefsetya@live.com>
2017-05-13 22:54:49 +0700

    update

--- resources/views/detail/rute.blade.php
+++ resources/views/detail/rute.blade.php
@@ -43,8 +43,7 @@
 												<tr>
 													<td class="long-text links-color" style="text-align: left; color: #3f3f3f;" align="left">
 														<p style="margin: 1em 0px; margin-bottom: 0px; margin-top: 0px;word-wrap: break-word;white-space: normal;">
-															{!!preg_replace('/((www|http:\/\/)[^ ]+)/', '<a href="\1" target="_blank">\1</a>', 
-    str_replace("<br>", "\n",$data->description))!!}
+															{!!preg_replace('/((www|http:\/\/)[^ ]+)/', '<a href="\1" target="_blank">\1</a>', $data->description)!!}
 														</p>
 													</td>
 												</tr>
